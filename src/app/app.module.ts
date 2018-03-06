b02f38a94b37f2426ee95c48a3fe67dc5b412cc1
Abhilash Reddy <abhilash.reddy@outlook.com>
2018-03-06 15:24:56 -0500

    Added ngBootstrap and added ToolTip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './DisplayingData/movies.component';
@@ -10,6 +11,7 @@ import { GenreService } from './Services/genre.service';
 import { SummaryPipe } from './pipes/summary.pipe';
 import { CapitalizePipe } from './pipes/capitalize.pipe';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -20,8 +22,10 @@ import { CapitalizePipe } from './pipes/capitalize.pipe';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule,
-    HttpModule
+    HttpModule,
+    NgbModule.forRoot()
   ],
   providers: [MovieService, GenreService],
   bootstrap: [AppComponent]
