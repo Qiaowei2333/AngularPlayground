0e45b06ccdfcb53cbd54e8493f82f087d36bbd4f
Reddy57 <abhilash.gaddam@antra.net>
2018-05-01 14:44:39 -0400

    added somrthing

--- src/app/movies/create-movie.component.ts
+++ src/app/movies/create-movie.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateMovieComponent implements OnInit {
 
+  movieName = 'Avengers 3';
   constructor() { }
 
   ngOnInit() {
   }
 
+  onKeyUp($event) {
+    if ($event.keyCode === 13) {
+      console.log('ENTER Key has been pressed!');
+      // we can use target.value to get the value
+      console.log($event.target.value);
+    }
+  }
+
+  onKeyUpEnter($event) {
+    console.log($event.target.value);
+  }
+
+  onKeyUpGetValueFromTemplate(templatevariable) {
+
+    console.log(templatevariable);
+  }
+
+  onKeyUpGetPropertyValue(xxx) {
+    console.log('property binding is one way, so no updated value');
+    console.log(xxx);
+  }
+  onKeyUpTwoWayBinding(dddd) {
+    console.log('ngmodel, banana in box is two way, so updated value is present');
+    console.log(dddd);
+  }
+  onClick() {
+    console.log('clicked!');
+  }
 }
