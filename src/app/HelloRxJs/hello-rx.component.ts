a31d2005327de91f374f32548dc04c34a3870533
Reddy57 <abhilash.gaddam@antra.net>
2018-06-13 13:56:27 -0400

    new code

--- src/app/HelloRxJs/hello-rx.component.ts
+++ src/app/HelloRxJs/hello-rx.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, fromEvent } from 'rxjs';
 import { map, take } from 'rxjs/operators';
 import { of } from 'rxjs';
 
@@ -14,13 +14,33 @@ export class HelloRxComponent implements OnInit {
 
   ngOnInit() {
 
-    const test = of(1, 2, 3).map(x =>  x + '!!!');
-    test.subscribe( x =>  console.log(x));
+    // const test = of(1, 2, 3).map(x =>  x + '!!!');
+    // test.subscribe( x =>  console.log(x));
 
-    const nums = of(1, 2, 3);
-    const squareValues = map((val: number) => val * val);
-    const squaredNums = squareValues(nums);
-    squaredNums.subscribe(x => console.log(x));
+    // const nums = of(1, 2, 3);
+    // const squareValues = map((val: number) => val * val);
+    // const squaredNums = squareValues(nums);
+    // squaredNums.subscribe(x => console.log(x));
+
+
+    // const el = document.getElementById('jumbo');
+
+    // // Create an Observable that will publish mouse movements
+    // const mouseMoves = fromEvent(el, 'mousemove');
+
+    // // Subscribe to start listening for mouse-move events
+    // const subscription = mouseMoves.subscribe((evt: MouseEvent) => {
+    //   // Log coords of mouse movements
+    //   console.log(`Coords: ${evt.clientX} X ${evt.clientY}`);
+
+    //   // When the mouse is over the upper-left of the screen,
+    //   // unsubscribe to stop listening for mouse movements
+    //   if (evt.clientX < 40 && evt.clientY < 40) {
+    //     subscription.unsubscribe();
+    //   }
+    // });
+
+
 
   }
 
