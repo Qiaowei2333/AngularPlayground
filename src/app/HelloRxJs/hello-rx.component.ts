08b274cc79003febeddc77bc3c529de468686561
Reddy57 <abhilash.gaddam@antra.net>
2018-06-13 14:14:18 -0400

    added rxjs fromEvent

--- src/app/HelloRxJs/hello-rx.component.ts
+++ src/app/HelloRxJs/hello-rx.component.ts
@@ -23,22 +23,22 @@ export class HelloRxComponent implements OnInit {
     // squaredNums.subscribe(x => console.log(x));
 
 
-    // const el = document.getElementById('jumbo');
+    const el = document.getElementById('btnp');
 
-    // // Create an Observable that will publish mouse movements
-    // const mouseMoves = fromEvent(el, 'mousemove');
+    // Create an Observable that will publish mouse movements
+    const mouseMoves = fromEvent(el, 'dblclick');
 
-    // // Subscribe to start listening for mouse-move events
-    // const subscription = mouseMoves.subscribe((evt: MouseEvent) => {
-    //   // Log coords of mouse movements
-    //   console.log(`Coords: ${evt.clientX} X ${evt.clientY}`);
+    // Subscribe to start listening for mouse-move events
+    const subscription = mouseMoves.subscribe((evt: MouseEvent) => {
+      // Log coords of mouse movements
+      console.log(`Coords: ${evt.clientX} X ${evt.clientY}`);
 
-    //   // When the mouse is over the upper-left of the screen,
-    //   // unsubscribe to stop listening for mouse movements
-    //   if (evt.clientX < 40 && evt.clientY < 40) {
-    //     subscription.unsubscribe();
-    //   }
-    // });
+      // When the mouse is over the upper-left of the screen,
+      // unsubscribe to stop listening for mouse movements
+      if (evt.clientX < 40 && evt.clientY < 40) {
+        subscription.unsubscribe();
+      }
+    });
 
 
 
